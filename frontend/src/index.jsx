626153dd759661cc1a106adc4d0a48ad111ca94a
rosechanapa <s6404062630279@email.kmutnb.ac.th>
2025-01-12 22:19:09 +0700

    socket index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,14 @@ import "../src/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// 1) import socket ที่คุณสร้างไว้
+import { socket } from "./socket";
+
+// 2) ทดสอบการเชื่อมต่อ
+socket.on("connect", () => {
+  console.log("Connected to socket server from index.jsx:", socket.id);
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -11,7 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
